9fe05fa3bf9205a380b2e5a7795ecfb9201e9168
anshuman619 <mishraanshuman619@gmail.com>
2023-12-20 15:49:27 +0530

    initial commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,30 +2,30 @@ import React from 'react';
 
 function Footer() {
   return (
-<footer class="shadow bg-blue-600  w-full">
-    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
-            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+<footer className="shadow bg-blue-600  w-full">
+    <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+            <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
             </a>
-            <ul class="flex flex-wrap items-center mb-6 xl:text-xl md:text-md sm:text-sm font-medium text-white sm:mb-0">
+            <ul className="flex flex-wrap items-center mb-6 xl:text-xl md:text-md sm:text-sm font-medium text-white sm:mb-0">
                 <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Services</a>
+                    <a href="#" className="hover:underline me-4 md:me-6">Services</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Events</a>
+                    <a href="#" className="hover:underline me-4 md:me-6">Events</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Media</a>
+                    <a href="#" className="hover:underline me-4 md:me-6">Media</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:underline">Contact Us</a>
+                    <a href="#" className="hover:underline">Contact</a>
                 </li>
             </ul>
         </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-        <span class="block xl:text-xl md:text-md sm:text-sm text-white sm:text-center">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+        <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+        <span className="block xl:text-xl md:text-md sm:text-sm text-white sm:text-center">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
     </div>
 </footer>
   )
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,13 +1,13 @@
 "use client"
 import React, { useEffect, useState } from "react";
-// import "./Slider.css";
+import "../components/styles/Slider.css";
 
 function Slider() {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentSlide((prevSlide) => (prevSlide + 1) % 5); // Adjust the number based on the number of slides
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % 10); // Adjust the number based on the number of slides
     }, 4000);
 
     return () => clearInterval(interval);
@@ -21,8 +21,8 @@ function Slider() {
         data-carousel="slide"
       >
         {/* Carousel wrapper */}
-        <div className="relative h-56 overflow-hidden   md:h-[580px]">
-          {[1, 2, 3, 4, 5,6,7,8,9,10].map((index) => (
+        <div className="relative h-56 overflow-hidden md:h-[580px]">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((index) => (
             <div
               key={index}
               className={`${
@@ -30,11 +30,16 @@ function Slider() {
               } duration-700 ease-in-out`}
               data-carousel-item
             >
-              <img
-                src={`/images/image${index}.jpg`}
-                className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                alt={`image${index}`}
-              />
+              {/* Overlay to prevent right-click */}
+              <div>
+                <img
+                  src={`/images/image${index}.jpg`}
+                  className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                  alt={`image${index}`}
+                  draggable="false" // Disable image dragging
+                />
+                
+              </div>
             </div>
           ))}
         </div>
@@ -61,32 +66,20 @@ function Slider() {
           className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
           data-carousel-prev
           onClick={() =>
-            setCurrentSlide((prevSlide) => (prevSlide - 1 + 5) % 5)
+            setCurrentSlide((prevSlide) => (prevSlide - 1 + 10) % 10)
           }
         >
-          {/* previous button */}
-          <span class="inline-flex items-center justify-center w-15 h-15 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-            </svg>
-            <span class="sr-only">Previous</span>
-        </span>
+          {/* ... (Previous button content) */}
         </button>
         <button
           type="button"
           className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
           data-carousel-next
           onClick={() =>
-            setCurrentSlide((prevSlide) => (prevSlide + 1) % 5)
+            setCurrentSlide((prevSlide) => (prevSlide + 1) % 10)
           }
         >
-          {/* Next button */}
-           <span class="inline-flex items-center justify-center w-15 h-15 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-            </svg>
-            <span class="sr-only">Next</span>
-        </span>
+          {/* ... (Next button content) */}
         </button>
       </div>
     </div>
